d547467454b1ffa710e3271c8d009f2bec7ea75e
1K0K01 <backupenter01@gmail.com>
2025-03-19 14:35:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const settings = {
         claude: [],
         openai: [],
         google: [],
+        perplexity: [],
     },
     openai_model: undefined,
     claude_model: undefined,
     google_model: undefined,
+    perplexity_model: undefined,
 };
 Object.assign(settings, extension_settings.customModels ?? {});
 // fix if installed before google support was added
